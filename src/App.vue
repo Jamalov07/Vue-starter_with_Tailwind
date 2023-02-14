4d2711e199b2669ec013359360f203687d72e8b2
Jamalov <Jamalovn07@gmail.com>
2023-02-14 16:35:18 +0500

    add starter

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1
+    class="w-full p-4 text-center bg-sky-700 text-bold text-3xl text-white uppercase"
+  >
+    {{ message }}
+  </h1>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {
+      message: "Vue.js starter with tailwind",
+    };
+  },
+};
+</script>
+
+<style></style>
